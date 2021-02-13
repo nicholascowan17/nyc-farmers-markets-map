30b648fdeef8fb366ac97b229a2692f66fc46430
nicholascowan17 <78500058+nicholascowan17@users.noreply.github.com>
2021-02-13 17:57:30 -0500

    Replace base marker with market icon

--- js/scripts.js
+++ js/scripts.js
@@ -11,22 +11,28 @@ var map = new mapboxgl.Map({
 var nav = new mapboxgl.NavigationControl();
 map.addControl(nav, 'top-left');
 
+var filterGroup = document.getElementById('filter-group');
+
 $.getJSON('./data/markets.json', function(markets) {
   markets.forEach(function(market) {
     var html = `
       <div>
         <h3>${market.Market_Name}</h3>
-        <div>Location: ${market.Street_Address}</div>
+        <div>Address: ${market.Street_Address}</div>
         <div>Hours of Operation: ${market.Days_of_Operation} ${market.Hours_of_Operations}</div>
       </div>
     `
 
-    new mapboxgl.Marker({
-      color: 'green',
-      scale: 0.8
-    })
+    // create a DOM element for the marker
+    var el = document.createElement('div');
+      el.className = 'marker';
+      el.style.backgroundImage = 'url("img/market-icon.png")';
+      el.style.width = '30px';
+      el.style.height = '30px';
+
+    new mapboxgl.Marker(el)
       .setLngLat([market.Longitude, market.Latitude])
       .setPopup(new mapboxgl.Popup().setHTML(html)) // add popup
       .addTo(map);
-  })
+  });
 })
