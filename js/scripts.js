4ca440ec95bc9fc5f415095fed45f4ac89c44b0c
nicholascowan17 <78500058+nicholascowan17@users.noreply.github.com>
2021-02-15 17:58:38 -0500

    Add toggle for days of operation

--- js/scripts.js
+++ js/scripts.js
@@ -9,9 +9,27 @@ var map = new mapboxgl.Map({
 
 // add navigation control in top left
 var nav = new mapboxgl.NavigationControl();
-map.addControl(nav, 'top-left');
+map.addControl(nav, 'top-right');
 
 var filterGroup = document.getElementById('filter-group');
+
+// Day toggles
+var toggleableDays = ['MONDAY', 'TUESDAY', 'WEDNESDAY', 'THURSDAY', 'FRIDAY', 'SATURDAY', 'SUNDAY'];
+
+// Add checkbox and label elements for the layer.
+toggleableDays.forEach(function(id) {
+  var input = document.createElement('input');
+    input.className = 'active';
+    input.type = 'checkbox';
+    input.id = id;
+    input.checked = true;
+    filterGroup.appendChild(input);
+
+  var label = document.createElement('label');
+    label.setAttribute('for', id);
+    label.textContent = id;
+    filterGroup.appendChild(label);
+  });
 
 $.getJSON('./data/markets.json', function(markets) {
   markets.forEach(function(market) {
@@ -26,6 +44,7 @@ $.getJSON('./data/markets.json', function(markets) {
     // create a DOM element for the marker
     var el = document.createElement('div');
       el.className = 'marker';
+      el.id = 'DO:' + market.Days_of_Operation;
       el.style.backgroundImage = 'url("img/market-icon.png")';
       el.style.width = '30px';
       el.style.height = '30px';
@@ -35,4 +54,74 @@ $.getJSON('./data/markets.json', function(markets) {
       .setPopup(new mapboxgl.Popup().setHTML(html)) // add popup
       .addTo(map);
   });
+})
+
+document.getElementById("MONDAY").addEventListener('change', function(e) {
+  if (e.target.checked === false) {
+    $('*[id*=Monday]').each(function() {
+      $(this).hide();});
+  } else {
+    $('*[id*=Monday]').each(function() {
+      $(this).show();});
+  }
+})
+
+document.getElementById("TUESDAY").addEventListener('change', function(e) {
+  if (e.target.checked === false) {
+    $('*[id*=Tuesday]').each(function() {
+      $(this).hide();});
+  } else {
+    $('*[id*=Tuesday]').each(function() {
+      $(this).show();});
+  }
+})
+
+document.getElementById("WEDNESDAY").addEventListener('change', function(e) {
+  if (e.target.checked === false) {
+    $('*[id*=Wednesday]').each(function() {
+      $(this).hide();});
+  } else {
+    $('*[id*=Wednesday]').each(function() {
+      $(this).show();});
+  }
+})
+
+document.getElementById("THURSDAY").addEventListener('change', function(e) {
+  if (e.target.checked === false) {
+    $('*[id*=Thursday]').each(function() {
+      $(this).hide();});
+  } else {
+    $('*[id*=Thursday]').each(function() {
+      $(this).show();});
+  }
+})
+
+document.getElementById("FRIDAY").addEventListener('change', function(e) {
+  if (e.target.checked === false) {
+    $('*[id*=Friday]').each(function() {
+      $(this).hide();});
+  } else {
+    $('*[id*=Friday]').each(function() {
+      $(this).show();});
+  }
+})
+
+document.getElementById("SATURDAY").addEventListener('change', function(e) {
+  if (e.target.checked === false) {
+    $('*[id*=Saturday]').each(function() {
+      $(this).hide();});
+  } else {
+    $('*[id*=Saturday]').each(function() {
+      $(this).show();});
+  }
+})
+
+document.getElementById("SUNDAY").addEventListener('change', function(e) {
+  if (e.target.checked === false) {
+    $('*[id*=Sunday]').each(function() {
+      $(this).hide();});
+  } else {
+    $('*[id*=Sunday]').each(function() {
+      $(this).show();});
+  }
 })
